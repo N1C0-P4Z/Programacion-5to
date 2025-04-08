7fd17da6955b9d5222776fa97163a951e09a28c4
akcjsbfifb <martinmoloeznik@gmail.com>
2025-04-08 11:09:55 -0300

    ej4 listo

--- shujman/shumi4.c
+++ shujman/shumi4.c
@@ -1,32 +1,51 @@
 #include <stdio.h>
 #include <stdlib.h>
-char hexaABinario(char hexa[2]);
+#include <string.h>
+#include <ctype.h>
+int hexaADec(char hexa);
 int elem(int n,char a, char array[n]);
+void hexAAscii(char *hexa);
 void imprimirBinario(char binary);
+void asciiAHexa(char *ascii);
 int main(){
 	char hexa[10];
 	printf("ingrese valor hexa: ");
 	scanf("%s", hexa);
-	printf("hexa: %s\n", hexa);
-	for(int i = 0; i<10; i+=2){
-		char doshexa[2] = {hexa[i], hexa[i]};
-		printf("doshexa: %i, i: %i, i++ %i, ++i %i, char: %c\n",doshexa, i, i++, ++i, hexaABinario(doshexa));
-	}
+	hexAAscii(hexa);
+	char ascii[10];
+	printf("ingrese valor ascii: ");
+	scanf("%s", ascii);
+	asciiAHexa(ascii);
 }
-char hexaABinario(char hexa[2]){
-	char resultado;
-	for(int i = 0; i<2;i++){
-		if(elem(10, hexa[i], "0123456789")){
-			hexa[i] -= 48;
-		}else if(elem(6, hexa[i], "abcdef")){
-			hexa[i] -= 87;
-		}else{
-			hexa[i] -= 55;
-		}	
+
+int hexaADec(char hexa){
+	    if (hexa >= '0' && hexa <= '9') return hexa - '0';
+	    hexa = tolower(hexa);
+	    if (hexa >= 'a' && hexa <= 'f') return hexa - 'a' + 10;
+	    return -1;
+}
+void hexAAscii(char *hexa){
+       int i = 0;
+	while(hexa[i] && hexa[i+1]){
+		int right = hexaADec(hexa[i]);
+		int left = hexaADec(hexa[i+1]);
+		char result = (right << 4) | left;
+		printf("%c", result);
+		i += 2;
 	}
-	resultado = hexa[0]<<4;
-	resultado |= hexa[1];
-	return resultado;
+	printf("\n");
+}
+char decAHexa(int num){
+	if(num<10) return '0' + num;
+	if(num>9 && num <16) return 'a' +num;
+	return 'x';
+}
+void asciiAHexa(char *ascii){
+	for(int i = 0; ascii[i]; i++){
+		char prim = decAHexa(ascii[i]>>4);
+		char sec = decAHexa(ascii[i] & 15);
+		printf("%c%c", prim, sec);
+	}
 }
 void imprimirBinario(char binary){
 		for(unsigned int m=1<<7; m; m >>= 1) 
