91007877985a10aa83b99d307d894f22c0b7d60b
N1C0-P4Z <rubenpaz2105@gmail.com>
2025-04-19 16:32:53 -0300

    ejercicio terminado

--- ejerciciovideo.c
+++ ejerciciovideo.c
@@ -2,8 +2,7 @@
 #include <stdlib.h>
 #include <string.h>
 
-#define MAX_ALUMNOS 100
-
+// Estructura de alumno
 typedef struct {
     char nombre[50];
     char apellido[50];
@@ -13,42 +12,116 @@ typedef struct {
     float promedio;
 } Alumno;
 
-Alumno alumnos[MAX_ALUMNOS];
-int cantidadAlumnos = 0;
+// Prototipos de las funciones
+void agregarAlumnos(Alumno array[], int cantidad);
+void mostrarTodos(Alumno array[], int cantidad);
+void buscarPorDNI(Alumno array[], int cantidad, int dni);
+void modificarAlumno(Alumno alumnos[], int cantidad, int dni);
+int eliminarAlumno(Alumno alumnos[], int cantidad, int dni);
 
-// Calcular promedio
+float calcularPromedio(int notas[]);
+
+
+int main(void) 
+{
+    int cantidad_estudiantes;
+    int dni, opcion;
+
+    printf("¿Cuántos alumnos desea agregar? ");
+    scanf("%d", &cantidad_estudiantes);
+
+    Alumno array[cantidad_estudiantes];
+
+    
+    do {
+        printf("\n--- MENU ---\n");
+        printf("1. Agregar alumnos\n");
+        printf("2. Buscar alumno por DNI\n");
+        printf("3. Modificar alumno\n");
+        printf("4. Eliminar alumno\n");
+        printf("5. Mostrar todos los alumnos\n");
+        printf("6. Salir\n");
+        printf("Ingrese una opción: ");
+        scanf("%d", &opcion);
+
+        switch(opcion) {
+            case 1:
+                agregarAlumnos(array, cantidad_estudiantes);
+                break;
+            case 2:
+                printf("Ingrese el DNI a buscar: ");
+                scanf("%d", &dni);
+                buscarPorDNI(array, cantidad_estudiantes, dni);
+                break;
+            case 3:
+                printf("Ingrese el DNI a modificar: ");
+                scanf("%d", &dni);
+                modificarAlumno(array, cantidad_estudiantes, dni);
+                break;
+            case 4:
+                printf("Ingrese el DNI a eliminar: ");
+                scanf("%d", &dni);
+                cantidad_estudiantes = eliminarAlumno(array, cantidad_estudiantes, dni);
+                break;
+            case 5:
+                mostrarTodos(array, cantidad_estudiantes);
+                break;
+            case 6:
+                printf("Saliendo del programa.\n");
+                break;
+            default:
+                printf("Opción inválida.\n");
+        }
+
+    } while(opcion != 6);
+
+    return 0;
+}
+
+// Definición de las funciones
+
 float calcularPromedio(int notas[]) {
     return (notas[0] + notas[1] + notas[2]) / 3.0;
 }
 
-// Agregar alumnos
-void agregarAlumnos(int n) {
-    int i;
-    for (i = 0; i < n && cantidadAlumnos < MAX_ALUMNOS; i++) {
-        printf("\nAlumno #%d\n", cantidadAlumnos + 1);
+
+void agregarAlumnos(Alumno alumnos[], int cantidad) {
+    for (int i = 0; i < cantidad; i++) {
+        printf("\nAlumno #%d\n", i + 1);
         printf("Nombre: ");
-        scanf("%s", alumnos[cantidadAlumnos].nombre);
+        scanf("%s", alumnos[i].nombre);
         printf("Apellido: ");
-        scanf("%s", alumnos[cantidadAlumnos].apellido);
+        scanf("%s", alumnos[i].apellido);
         printf("DNI: ");
-        scanf("%d", &alumnos[cantidadAlumnos].dni);
+        scanf("%d", &alumnos[i].dni);
         printf("Edad: ");
-        scanf("%d", &alumnos[cantidadAlumnos].edad);
-        
+        scanf("%d", &alumnos[i].edad);
+
         for (int j = 0; j < 3; j++) {
             printf("Nota %d: ", j + 1);
-            scanf("%d", &alumnos[cantidadAlumnos].notas[j]);
+            scanf("%d", &alumnos[i].notas[j]);
         }
 
-        alumnos[cantidadAlumnos].promedio = calcularPromedio(alumnos[cantidadAlumnos].notas);
-        cantidadAlumnos++;
+        alumnos[i].promedio = calcularPromedio(alumnos[i].notas);
     }
 }
 
-// Buscar por DNI
-void buscarPorDNI(int dni) {
+void mostrarTodos(Alumno alumnos[], int cantidad) 
+{
+    for (int i = 0; i < cantidad; i++) {
+        printf("\nAlumno #%d\n", i + 1);
+        printf("Nombre: %s\n", alumnos[i].nombre);
+        printf("Apellido: %s\n", alumnos[i].apellido);
+        printf("DNI: %d\n", alumnos[i].dni);
+        printf("Edad: %d\n", alumnos[i].edad);
+        printf("Notas: %d, %d, %d\n", alumnos[i].notas[0], alumnos[i].notas[1], alumnos[i].notas[2]);
+        printf("Promedio: %.2f\n", alumnos[i].promedio);
+    }
+}
+
+void buscarPorDNI(Alumno alumnos[], int cantidad, int dni) {
     int encontrado = 0;
-    for (int i = 0; i < cantidadAlumnos; i++) {
+    for (int i = 0; i < cantidad; i++) {
         if (alumnos[i].dni == dni) {
             printf("\nAlumno encontrado:\n");
             printf("Nombre: %s\n", alumnos[i].nombre);
@@ -58,7 +131,6 @@ void buscarPorDNI(int dni) {
             printf("Notas: %d, %d, %d\n", alumnos[i].notas[0], alumnos[i].notas[1], alumnos[i].notas[2]);
             printf("Promedio: %.2f\n", alumnos[i].promedio);
             encontrado = 1;
-            break;
         }
     }
     if (!encontrado) {
@@ -66,10 +138,9 @@ void buscarPorDNI(int dni) {
     }
 }
 
-// Modificar alumno
-void modificarAlumno(int dni) {
+void modificarAlumno(Alumno alumnos[], int cantidad, int dni) {
     int encontrado = 0;
-    for (int i = 0; i < cantidadAlumnos; i++) {
+    for (int i = 0; i < cantidad; i++) {
         if (alumnos[i].dni == dni) {
             printf("Modificar datos del alumno:\n");
             printf("Nuevo nombre: ");
@@ -94,89 +165,22 @@ void modificarAlumno(int dni) {
     }
 }
 
-// Eliminar alumno
-void eliminarAlumno(int dni) {
+int eliminarAlumno(Alumno alumnos[], int cantidad, int dni) {
     int encontrado = 0;
-    for (int i = 0; i < cantidadAlumnos; i++) {
+    for (int i = 0; i < cantidad; i++) {
         if (alumnos[i].dni == dni) {
-            for (int j = i; j < cantidadAlumnos - 1; j++) {
+            for (int j = i; j < cantidad - 1; j++) {
                 alumnos[j] = alumnos[j + 1];
             }
-            cantidadAlumnos--;
+            cantidad--;
             printf("Alumno eliminado.\n");
             encontrado = 1;
-            break;
+            break; 
         }
     }
     if (!encontrado) {
         printf("Alumno no encontrado.\n");
     }
+    return cantidad;
 }
 
-// Mostrar todos
-void mostrarTodos() {
-    if (cantidadAlumnos == 0) {
-        printf("No hay alumnos cargados.\n");
-    } else {
-        for (int i = 0; i < cantidadAlumnos; i++) {
-            printf("\nAlumno #%d\n", i + 1);
-            printf("Nombre: %s\n", alumnos[i].nombre);
-            printf("Apellido: %s\n", alumnos[i].apellido);
-            printf("DNI: %d\n", alumnos[i].dni);
-            printf("Edad: %d\n", alumnos[i].edad);
-            printf("Notas: %d, %d, %d\n", alumnos[i].notas[0], alumnos[i].notas[1], alumnos[i].notas[2]);
-            printf("Promedio: %.2f\n", alumnos[i].promedio);
-        }
-    }
-}
-
-// Menú principal
-int main() {
-    int opcion, dni, cantidad;
-
-    do {
-        printf("\n--- MENU ---\n");
-        printf("1. Agregar alumnos\n");
-        printf("2. Buscar alumno por DNI\n");
-        printf("3. Modificar alumno\n");
-        printf("4. Eliminar alumno\n");
-        printf("5. Mostrar todos los alumnos\n");
-        printf("6. Salir\n");
-        printf("Ingrese una opción: ");
-        scanf("%d", &opcion);
-
-        switch(opcion) {
-            case 1:
-                printf("¿Cuántos alumnos desea agregar? ");
-                scanf("%d", &cantidad);
-                agregarAlumnos(cantidad);
-                break;
-            case 2:
-                printf("Ingrese el DNI a buscar: ");
-                scanf("%d", &dni);
-                buscarPorDNI(dni);
-                break;
-            case 3:
-                printf("Ingrese el DNI a modificar: ");
-                scanf("%d", &dni);
-                modificarAlumno(dni);
-                break;
-            case 4:
-                printf("Ingrese el DNI a eliminar: ");
-                scanf("%d", &dni);
-                eliminarAlumno(dni);
-                break;
-            case 5:
-                mostrarTodos();
-                break;
-            case 6:
-                printf("Saliendo del programa.\n");
-                break;
-            default:
-                printf("Opción inválida.\n");
-        }
-
-    } while(opcion != 6);
-
-    return 0;
-}
