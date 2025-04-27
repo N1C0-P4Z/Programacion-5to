5b13cd66d7b2a2f10d77f619c4a37821a5308590
N1C0-P4Z <rubenpaz2105@gmail.com>
2025-04-27 00:06:44 -0300

    me quede en el ejercicio 6 de folds

--- ejercicios de clases/haskell/haskellnico.hs
+++ ejercicios de clases/haskell/haskellnico.hs
@@ -111,3 +111,55 @@ llamarDiv a1 a2 = transformar (divisionSegura a1 a2)
     where
         transformar (Just r) = Left r
         transformar Nothing = Right "Division por cero"
+
+
+-- PRACTICA DE FOLDS
+
+--1
+
+foldNat :: (r -> r) -> r -> Nat -> r
+foldNat f b Zero = b
+foldNat f b (Succ n) = f (foldNat f b n)
+
+foldList :: (a -> r -> r) -> r -> List a -> r
+foldList f b Nil = b
+foldList f b (Cons x xs) = f x (foldList f b xs)
+
+--2
+
+nat2int2 :: Nat -> Int
+nat2int2 nro = foldNat (1 +) 0 nro
+
+
+sumar2' :: List Int -> Int
+sumar2' (Cons x xs) = foldList (+) 0 (Cons x xs)
+
+list2alg2 :: [a] -> List a
+list2alg2 = foldr Cons Nil
+
+--3
+
+foldExpr :: (r -> r -> r) -> (r -> r -> r) -> (Int -> r) -> Expr -> r
+foldExpr s r l (Literal n) = l n
+foldExpr s r l (Suma n1 n2) = s (foldExpr s r l n1) (foldExpr s r l n2)
+foldExpr s r l (Resta n1 n2) = r (foldExpr s r l n1) (foldExpr s r l n2)
+
+--4
+
+mostrar2 :: Expr -> String
+mostrar2 = foldExpr (\ x y -> "(" ++ x ++ "+" ++ y ++ ")") (\ x y -> "(" ++ x ++ "-" ++ y ++ ")") show 
+
+evaluar2 :: Expr -> Int
+evaluar2 = foldExpr (+) (-) id
+
+buscar2 :: Int -> Expr -> Bool
+buscar2 k = foldExpr (\ x y -> x || y) (\ x y -> x || y) (== k)
+
+--5
+
+natSize :: List a -> Nat
+natSize l = foldList (\a x -> Succ x) Zero l
+
+--6
+
+--dup :: List a -> List a
