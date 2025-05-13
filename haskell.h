e831dff372528a366d2cd07e384ce93dbc31ac6e
N1C0-P4Z <rubenpaz2105@gmail.com>
2025-05-13 14:23:20 -0300

    ahora esta todo clean

--- haskell.h
+++
@@ -1,9 +0,0 @@
-data Mes = Enero | Febrero | Marzo | Abril | Mayo | Junio | Julio | Agosto | Septiembre | Octubre | Noviembre | Diciembre
-
-vacaciones :: Mes -> Bool
-vacaciones Enero = False
-vacaciones Febrero = False
-vacaiones Julio = False
-vacaiones Diciembre = False
-vacaiones otherwise = True
-
